0b707a7597c483628c79ff7a171866761114ac38
CaiowRG <caioribgon@gmail.com>
2024-11-15 09:08:03 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import {useEffect, useState} from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [pokemons, setPokemons] = useState({})
+
+  const getPokemons = (id) => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).
+    then(response => {
+      const pokemon = response.data
+      setPokemons((prevPokemon) =>({...prevPokemon, [id]:pokemon}))
+    }) 
+  }
+
+  const arrayPokemons = () => Array(150).fill().
+  map((_, index) => getPokemons(index + 1))
+
+  useEffect(() => {
+    arrayPokemons()
+  }, [])
+  console.log('deu certo', pokemons)
+
+  return (
+      <div>
+       <h1>Pokedex</h1>
+       <ul>
+        {
+          Object.values(pokemons).map(item =>
+            <li>
+             {item.id}. {item.name}
+             <img src={item.sprites.front_default} alt="" />
+             {item.types.map(item => item.type.name).join(" || ")}
+              
+              
+
+            </li>
+            ) 
+        }
+       </ul>
+      </div>
+  )
+}
+
+export default App
